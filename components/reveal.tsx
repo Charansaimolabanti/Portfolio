cc484a0260a3ab12b3d596caf2956a5042b914ce
Charansaimolabanti <charansai.molabanti@gmail.com>
2025-09-27 02:08:51 +0530

    error fix

--- components/reveal.tsx
+++ components/reveal.tsx
@@ -2,8 +2,7 @@
 import React from "react";
 import {
   TextRevealCard,
-  TextRevealCardDescription,
-  TextRevealCardTitle,
+  
 } from "../components/ui/text-reveal-card";
 
 export function TextRevealCardPreview() {
